6df850cb1269108d14f11fe136de68397c7fd9c6
rodroaro <rod_roaro_13@hotmail.com>
2022-05-16 21:04:39 -0500

    .

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   resources :users, except: %i[new]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessións$destroy'
+  delete 'logout', to: 'sessions#destroy'
 end
